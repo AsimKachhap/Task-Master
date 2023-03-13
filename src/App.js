dac4fbc8383ab3a214f10bd956b7c5d0d383ea93
unknown <asim26k@gmail.com>
2023-03-13 23:23:52 +0530

    Implemented basic routing and made components fordaily, weekly, monthly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DailyPage from "./components/DailyPage";
+import WeeklyPage from "./components/WeeklyPage";
+import MonthlyPage from "./components/MonthlyPage";
 
 function App() {
-  return <div className="App text-rose-500">Hello World !!!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DailyPage />} />
+        <Route path="/weekly" element={<WeeklyPage />} />
+        <Route path="/monthly" element={<MonthlyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
