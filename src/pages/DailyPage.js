73591b5ac57f5d052b4c82f6e47f6a64d6be11b3
unknown <asim26k@gmail.com>
2023-03-16 02:55:08 +0530

    Added space between Differrent DailyTaskBoards.

--- src/pages/DailyPage.js
+++ src/pages/DailyPage.js
@@ -3,7 +3,7 @@ import DailyTaskBoard from "../components/DailyTaskBoard";
 
 const DailyPage = () => {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 gap-2 rounded-md ">
       <DailyTaskBoard heading="Tasks to Do" pinColor="#45BFFF" />
       <DailyTaskBoard heading="Tasks in Progress" pinColor="#FFBB55" />
       <DailyTaskBoard heading="Tasks Completed" pinColor="#3DED97" />
